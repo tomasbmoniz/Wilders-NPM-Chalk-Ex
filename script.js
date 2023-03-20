45c8ce33a39cc636473e71a1f0137f5aae161fc5
Tomás Botelho Moniz <tbotelhomonizlg@gmail.com>
2023-03-20 14:59:19 +0000

    Added node_modules to gitignore & updated the exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.blueBright('Tomas Gonzales'));
+
+console.log(chalk.yellow('Luis Ferri'));
+
+console.log(chalk.green('Jose Neves'));
+
+console.log(chalk.grey('Nuno Colaco'));
